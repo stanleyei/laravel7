cb4f778b3ebd500b35e6e6e4205604137802e225
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2021-05-06 15:51:59 +0800

    Update news_edit_page.blade.php

--- resources/views/news/news_edit_page.blade.php
+++ resources/views/news/news_edit_page.blade.php
@@ -6,21 +6,22 @@
 
 @section('main')
     <hr>
-    <form action="">
-        <label for="title_box">
-            標題：<input type="text" id="title_box">
+    <form action="/news/store" method="POST">
+        @csrf
+        <label for="title">
+            標題：<input type="text" id="title" name="title">
         </label>
-        <label for="date_box">
-            日期：<input type="date" id="date_box">
+        <label for="date">
+            日期：<input type="date" id="date" name="date">
         </label>
-        <label for="img_box">
-            圖片：<input type="text" id="img_box" placeholder=https://www.taiwan.net.tw/pic.ashx?qp=/0040119/13_0040119_1.jpg&sizetype=2>
+        <label for="img">
+            圖片：<input type="text" id="img" name="img" placeholder=https://www.taiwan.net.tw/pic.ashx?qp=/0040119/13_0040119_1.jpg&sizetype=2>
         </label>
-        <label for="content_box">
-            內文：<textarea name="" id="" cols="151" rows="10"></textarea>
+        <label for="content">
+            內文：<textarea name="content" id="" cols="151" rows="10"></textarea>
         </label>
-        <label for="views_box">
-            瀏覽數：<input type="number" id="views_box" value="0">
+        <label for="views">
+            瀏覽數：<input type="number" id="views" value="0" name="views">
         </label>
         <button type="submit" class="submit">送出</button>
     </form>
