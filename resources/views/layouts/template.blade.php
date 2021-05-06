9f40c2a34c7ec9bdbdaff266999ae548815817e9
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2021-05-06 21:53:49 +0800

    Update template.blade.php

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -71,7 +71,7 @@
             <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link mr-2 text-center" href="/news/list">News<span class="sr-only">(current)</span></a>
+                        <a class="nav-link mr-2 text-center" href="/news">News<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-body ml-md-4 text-center" href="#">Portfolio</a>
