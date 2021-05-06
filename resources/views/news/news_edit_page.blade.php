b54f6362e9f074d9150eaf711d65ebb7d20326e9
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2021-05-06 22:45:04 +0800

    Update news_edit_page.blade.php

--- resources/views/news/news_edit_page.blade.php
+++ resources/views/news/news_edit_page.blade.php
@@ -6,19 +6,19 @@
 
 @section('main')
     <hr>
-    <form action="/news/update/{{$newsData->id}}" method="POST">
+    <form action="{{isset($newsData) ? '/news/update/$newsData->id' : '/news/store'}}" method="POST">
         @csrf
         <label for="title">
-            標題：<input type="text" id="title" name="title"  value="{{$newsData->title}}">
+            標題：<input type="text" id="title" name="title"  value="{{$newsData->title ?? ''}}">
         </label>
         <label for="date">
-            日期：<input type="date" id="date" name="date" value="{{$newsData->date}}">
+            日期：<input type="date" id="date" name="date" value="{{$newsData->date ?? ''}}">
         </label>
         <label for="img">
-            圖片：<input type="text" id="img" name="img" value="{{$newsData->img}}">
+            圖片：<input type="text" id="img" name="img" value="{{$newsData->img ?? ''}}">
         </label>
         <label for="content">
-            內文：<textarea name="content" id="content" cols="30" rows="10">{{$newsData->content}}</textarea>
+            內文：<textarea name="content" id="content" cols="30" rows="10">{{$newsData->content ?? ''}}</textarea>
         </label>
         <button type="submit" class="submit">送出</button>
     </form>
