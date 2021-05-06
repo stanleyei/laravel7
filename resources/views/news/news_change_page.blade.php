dc8849f877d46eaa48fa76abf4de27c8e1b8c36e
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2021-05-06 09:40:30 +0800

    Update news_change_page.blade.php

--- resources/views/news/news_change_page.blade.php
+++ resources/views/news/news_change_page.blade.php
@@ -7,21 +7,21 @@
 @section('main')
     <form action="">
         <label for="title_box">
-            標題 <input type="text" id="title_box">
+            標題：<input type="text" id="title_box">
         </label>
         <label for="date_box">
-            日期 <input type="date" id="date_box">
+            日期：<input type="date" id="date_box">
         </label>
         <label for="img_box">
-            圖片 <input type="text" id="img_box">
+            圖片：<input type="text" id="img_box" placeholder=https://www.taiwan.net.tw/pic.ashx?qp=/0040119/13_0040119_1.jpg&sizetype=2>
         </label>
         <label for="content_box">
-            內文 <input type="text" id="content_box">
+            內文：<input type="text" id="content_box">
         </label>
         <label for="views_box">
-            瀏覽數 <input type="number" id="views_box">
+            瀏覽數：<input type="number" id="views_box" value="0">
         </label>
-        <button type="submit">更改文章</button>
+        <button type="submit">送出</button>
     </form>
 @endsection
 
