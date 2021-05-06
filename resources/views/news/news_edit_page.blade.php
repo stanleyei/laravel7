be03f5dcf23f2f15b37c66d7cc06ccbd034ab5f9
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2021-05-06 21:12:53 +0800

    Update news_edit_page.blade.php

--- resources/views/news/news_edit_page.blade.php
+++ resources/views/news/news_edit_page.blade.php
@@ -6,19 +6,19 @@
 
 @section('main')
     <hr>
-    <form action="/news/update/{{$newsData[0]->id}}" method="POST">
+    <form action="/news/update/{{$newsData->id}}" method="POST">
         @csrf
         <label for="title">
-            標題：<input type="text" id="title" name="title">
+            標題：<input type="text" id="title" name="title"  value="{{$newsData->title}}">
         </label>
         <label for="date">
-            日期：<input type="date" id="date" name="date">
+            日期：<input type="date" id="date" name="date" value="{{$newsData->date}}">
         </label>
         <label for="img">
-            圖片：<input type="text" id="img" name="img" placeholder=https://www.taiwan.net.tw/pic.ashx?qp=/0040119/13_0040119_1.jpg&sizetype=2>
+            圖片：<input type="text" id="img" name="img" value="{{$newsData->img}}">
         </label>
         <label for="content">
-            內文：<textarea name="content" id="" cols="151" rows="10"></textarea>
+            內文：<textarea name="content" id="content" cols="151" rows="10">{{$newsData->content}}</textarea>
         </label>
         <button type="submit" class="submit">送出</button>
     </form>
