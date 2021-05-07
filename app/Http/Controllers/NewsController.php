f5eaefa8016f3541eb3c4a44e0fc12ac46cfcd8d
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2021-05-07 15:32:18 +0800

    Update NewsController.php

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -15,7 +15,7 @@ class NewsController extends Controller
 
     public function index()
     {
-        return view('index');
+        return view('welcome');
     }
 
     public function newsList()
