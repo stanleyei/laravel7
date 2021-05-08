13c65a020b8fe548af06dd8907c7356c9216b411
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2021-05-08 19:50:18 +0800

    Update news_edit_page.blade.php

--- resources/views/news/news_edit_page.blade.php
+++ resources/views/news/news_edit_page.blade.php
@@ -46,18 +46,17 @@
 @endsection
 
 @section('js')
-
     <script>
-        const btn_delete = document.querySelectorAll('.btn_delete');
-        console.log(btn_delete);
-        btn_delete.forEach(btn =>
-            btn.addEventListener('click',e =>{
-                if(confirm('確定要刪除嗎?')){
-                    location.href = `${e.target.dataset.href}`;
-                }
-            }) 
-        )
+        window.onload = () => {
+            const btn_delete = document.querySelectorAll('.btn_delete');
+            btn_delete.forEach(btn => {
+                btn.addEventListener('click', () => {
+                    if(confirm('確定要刪除嗎?')){
+                        location.href = `${e.target.dataset.href}`;
+                    }
+                })
+            })
+        }
     </script>
-
 @endsection
 
