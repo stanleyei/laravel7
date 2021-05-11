862e2be70d0a7ef3c8e62583d55872fdbdd662f9
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2021-05-11 21:02:42 +0800

    Update products_create.blade.php

--- resources/views/admin/products_create.blade.php
+++ resources/views/admin/products_create.blade.php
@@ -5,20 +5,19 @@
 @endsection
 
 @section('main')
-    <hr>
     <form action="/admin/products/" method="POST" enctype="multipart/form-data">
         @csrf
         <label for="name">
-            產品名稱：<input type="text" id="title" name="name">
+            商品名稱：<input type="text" id="title" name="name">
         </label>
         <label for="img">
-            產品照片：<input type="file" id="date" name="img" accept="image/*">
+            商品照片：<input type="file" id="date" name="img" accept="image/*">
         </label>
         <label for="price">
-            產品價格：<input type="number" id="img" name="price">
+            商品價格：<input type="number" id="img" name="price">
         </label>
         <label for="content">
-            產品簡介：<textarea name="content" id="content" cols="30" rows="10"></textarea>
+            商品簡介：<textarea name="content" id="content" cols="30" rows="10"></textarea>
         </label>
         <button type="submit" class="submit">送出</button>
     </form>
