8b4f04fdf8dac2da7f42ccf4d69012764a299e4b
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2021-05-11 13:46:44 +0800

    Update AdminProductsController.php

--- app/Http/Controllers/AdminProductsController.php
+++ app/Http/Controllers/AdminProductsController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Products;
 use Illuminate\Http\Request;
 
 class AdminProductsController extends Controller
 {
     //
+    public function index()
+    {
+        $productsData = Products::get();
+        $total = Products::count();
+        return view('front.products_list_page', compact('productsData','total'));
+    }
 }
