e8e2974c02b6b3af3efe62876b88bc3dca52df9d
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2021-05-11 20:56:16 +0800

    Update AdminProductsController.php

--- app/Http/Controllers/AdminProductsController.php
+++ app/Http/Controllers/AdminProductsController.php
@@ -12,22 +12,30 @@ class AdminProductsController extends Controller
     {
         $productsData = Products::get();
         $total = Products::count();
-        return view('front.products_list_page', compact('productsData','total'));
+        return view('admin.products_edit_page', compact('productsData','total'));
     }
 
     public function show()
     {
-        
+        return view('admin.products_create');
     }
 
     public function create()
     {
-        
+        return view('admin.products_create');
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        $requsetData = $request->all();
         
+        if($request->hasFile('img')) {
+            $file = $request->file('img');
+            $path = $this->fileUpload($file,'products');
+            $requsetData['img'] = $path;
+        }
+        Products::create($requsetData);
+        return  redirect('/admin/products/');
     }
 
     public function update()
@@ -38,5 +46,24 @@ class AdminProductsController extends Controller
     public function delete()
     {
         
+    }
+
+    private function fileUpload($file,$dir){
+        //防呆：資料夾不存在時將會自動建立資料夾，避免錯誤
+        if( ! is_dir('upload/')){
+            mkdir('upload/');
+        }
+        //防呆：資料夾不存在時將會自動建立資料夾，避免錯誤
+        if ( ! is_dir('upload/'.$dir)) {
+            mkdir('upload/'.$dir);
+        }
+        //取得檔案的副檔名
+        $extension = $file->getClientOriginalExtension();
+        //檔案名稱會被重新命名
+        $filename = strval(time().md5(rand(100, 200))).'.'.$extension;
+        //移動到指定路徑
+        move_uploaded_file($file, public_path().'/upload/'.$dir.'/'.$filename);
+        //回傳 資料庫儲存用的路徑格式
+        return '/upload/'.$dir.'/'.$filename;
     }
 }
