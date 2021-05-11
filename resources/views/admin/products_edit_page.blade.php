42f189ba6cdf0f406955512a1d5fb6a83e802619
Stanley <81503453+stanleyei@users.noreply.github.com>
2021-05-11 23:57:37 +0800

    Update products_edit_page.blade.php

--- resources/views/admin/products_edit_page.blade.php
+++ resources/views/admin/products_edit_page.blade.php
@@ -5,19 +5,20 @@
 @endsection
 
 @section('main')
-    <form action="/admin/products/" method="POST" enctype="multipart/form-data">
+    <form action="/admin/products/{{$productsData->id}}" method="POST" enctype="multipart/form-data">
         @csrf
+        @method('PATCH')
         <label for="name" class="mb-3">
-            商品名稱：<input type="text" id="name" name="name">
+            商品名稱：<input type="text" id="name" name="name" value="{{$productsData->name}}">
         </label>
         <label for="img" class="mb-3">
             商品照片：<input type="file" id="img" name="img" accept="image/*">
         </label>
         <label for="price" class="mb-3">
-            商品價格：<input type="number" id="price" name="price">
+            商品價格：<input type="number" id="price" name="price" value="{{$productsData->price}}">
         </label>
         <label for="content">
-            商品簡介：<textarea name="content" id="content" cols="30" rows="10"></textarea>
+            商品簡介：<textarea name="content" id="content" cols="30" rows="10">{{$productsData->content}}</textarea>
         </label>
         <button type="submit" class="submit">送出</button>
     </form>
