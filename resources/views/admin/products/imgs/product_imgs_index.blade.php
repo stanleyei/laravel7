29a993f53ef0b70e82bb4e603d40cf16f37e64fd
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2021-05-13 20:25:35 +0800

    Update product_imgs_index.blade.php

--- resources/views/admin/products/imgs/product_imgs_index.blade.php
+++ resources/views/admin/products/imgs/product_imgs_index.blade.php
@@ -11,22 +11,22 @@
         <thead>
             <tr>
                 <th>編號</th>
-                <th>類別名稱</th>
-                <th>數量</th>
+                <th>產品編號</th>
+                <th>路徑</th>
                 <th>功能</th>
             </tr>
         </thead>
         <tbody>
-            @foreach ($typesData as $type)
+            @foreach ($imgsData as $img)
             <tr>
-                <td>{{$type->id}}</td>
-                <td>{{$type->name}}</td>
-                <td>{{$type->products->count()}}</td>
+                <td>{{$img->id}}</td>
+                <td>{{$img->product_id}}</td>
+                <td>{{$img->path}}</td>
                 <td>
-                    <a href="/admin/product_types/{{$type->id}}/edit" class="rounded-pill btn btn-success mr-2">編輯</a>
+                    <a href="/admin/product_types/{{$img->id}}/edit" class="rounded-pill btn btn-success mr-2">編輯</a>
                     <button type="button" class="btn_delete rounded-pill btn btn-danger"
-                        data-id="#delete_{{$type->id}}">刪除</button>
-                    <form id="delete_{{$type->id}}" action="/admin/product_types/{{$type->id}}" method="POST">
+                        data-id="#delete_{{$img->id}}">刪除</button>
+                    <form id="delete_{{$img->id}}" action="/admin/product_types/{{$img->id}}" method="POST">
                         @csrf
                         @method('DELETE')
                     </form>
