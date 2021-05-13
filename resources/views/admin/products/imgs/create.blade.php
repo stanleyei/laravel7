7169f30444debaa7261713bf7c07d259dbef39e6
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2021-05-13 20:30:50 +0800

    Create create.blade.php

---
+++ resources/views/admin/products/imgs/create.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/news_create.css') }}">
+@endsection
+
+@section('main')
+    <form action="/admin/product_imgs" method="POST" enctype="multipart/form-data">
+        @csrf
+        <label for="product_id" class="mb-3">
+            產品編號：<input type="text" id="product_id" name="product_id">
+        </label>
+        <label for="path" class="mb-3">
+            路徑：<input type="text" id="path" name="path">
+        </label>
+        <button type="submit" class="submit">送出</button>
+    </form>
+@endsection
+
+@section('js')
+    
+@endsection
