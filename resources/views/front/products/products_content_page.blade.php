d145eca29ffbf2cef2dd17f99eed6f81b5b03a36
Stanley <81503453+stanleyei@users.noreply.github.com>
2021-05-17 22:54:45 +0800

    Update products_content_page.blade.php

--- resources/views/front/products/products_content_page.blade.php
+++ resources/views/front/products/products_content_page.blade.php
@@ -10,6 +10,18 @@
     width: 600px;
     word-break: break-all;
   }
+
+  .figure{
+    width: 130px;
+    height: 130px;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center
+  }
+
+  .figure:hover{
+    border: 2px black solid;
+  }
 </style>
 @endsection
 
@@ -17,7 +29,14 @@
 <hr>
 <section class="container px-5">
   <div class="d-flex text-center justify-content-around">
-    <img src="{{asset($productsData->img)}}">
+    <div>
+      <img src="{{asset($productsData->img)}}">
+      <div class="d-flex mt-5">
+      @foreach ($productsData->productImgs as $img)
+        <a class="figure" type="button" style="background-image: url({{asset($img->img)}})"></a>
+      @endforeach
+      </div>
+    </div>
     <div class="px-3 pt-5 border-left">
       <h2 class="h1">{{$productsData->name}}</h2>
       <p class="my-5 h3">Price：$ {{$productsData->price}}</p>
