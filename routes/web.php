0ee7e07e682f462c127d0a37dd4ef320cba3429c
Stanley <81503453+stanleyei@users.noreply.github.com>
2021-05-19 16:54:53 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,8 @@ Route::prefix('news')->group(function () {
 });
 
 Route::prefix('products')->group(function () {
-    Route::get('/', 'ProductsController@index');
     Route::get('/content/{id}', 'ProductsController@content');
+    Route::get('/', 'ProductsController@index');
 });
 
 
