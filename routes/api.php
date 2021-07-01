0eefdbc074c35462e5eeb39927acb1650c188c66
Stanley <81503453+stanleyei@users.noreply.github.com>
2021-07-01 15:20:48 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/line_bot', 'LineBotController@response');
+Route::post('/line_bot', 'LineBotController@index');
