8e693dcf6cc191565d073846a8f356ec3fac92cb
Stanley <81503453+stanleyei@users.noreply.github.com>
2021-07-01 17:29:35 +0800

    Update LineBotController.php

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -11,22 +11,22 @@ class LineBotController extends Controller
 {
     public function index(Request $request)
     {
-        // $text = $request->events[0]['message']['text'] ?? '';
-        // error_log($text);
+        $text = $request->events[0]['message']['text'] ?? '';
+        error_log($text);
         $replyToken = $request['events'][0]['replyToken'];
 
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/json',
-            'Authorization' => 'Bearer ' .env('LINE_CHANNEL_ASCCES_TOKEN')
-        ])->post('https://api.line.me/v2/bot/message/reply' ,[
-            'replyToken' => $replyToken,
-            'messages' => [
-                [
-                    'type' => 'text',
-                    'text' => 'hello'
-                ]
-            ]
-        ]);
+        // $response = Http::withHeaders([
+        //     'Content-Type' => 'application/json',
+        //     'Authorization' => 'Bearer ' .env('LINE_CHANNEL_ASCCES_TOKEN')
+        // ])->post('https://api.line.me/v2/bot/message/reply' ,[
+        //     'replyToken' => $replyToken,
+        //     'messages' => [
+        //         [
+        //             'type' => 'text',
+        //             'text' => 'hello'
+        //         ]
+        //     ]
+        // ]);
 
         return response('ok', '200');
     }
